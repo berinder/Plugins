4c82d825e2a2f45f5a15e124eddbf2527450340a
Leif Ershag <leif.ershag@tieto.com>
2017-12-12 11:11:31 +0100

    More open in browser files.

---
+++ src/AnyStatus.Plugins/VSTS/Release/OpenVstsEnvironmentPage.cs
@@ -0,0 +1,24 @@
+﻿using AnyStatus.API;
+using System;
+
+namespace AnyStatus
+{
+    class OpenVstsEnvironmentPage : IOpenInBrowser<VSTSReleaseEnvironment>
+    {
+        private readonly IProcessStarter _processStarter;
+
+        public OpenVstsEnvironmentPage(IProcessStarter processStarter)
+        {
+            _processStarter = Preconditions.CheckNotNull(processStarter, nameof(processStarter));
+        }
+        public void Handle(VSTSReleaseEnvironment environment)
+        {
+            VSTSRelease_v1 parent = (VSTSRelease_v1) environment.Parent;
+            if (parent.DefinitionId == null)
+                throw new InvalidOperationException("Cannot open web page. Unknown definition id.");
+            var uri = $"{parent.Url}/{parent.Collection}/{parent.Project}/_release?definitionId={parent.DefinitionId}&definitionEnvironmentId={environment.DefinitionEnvironmentId}&_a=environment-summary";
+
+            _processStarter.Start(uri);
+        }
+    }
+}
---
+++ src/AnyStatus.Plugins/VSTS/Release/OpenVstsTaskPage.cs
@@ -0,0 +1,26 @@
+﻿using AnyStatus.API;
+using System;
+
+namespace AnyStatus
+{
+    class OpenVstsTaskPage : IOpenInBrowser<VSTSReleaseTask>
+    {
+        private readonly IProcessStarter _processStarter;
+
+        public OpenVstsTaskPage(IProcessStarter processStarter)
+        {
+            _processStarter = Preconditions.CheckNotNull(processStarter, nameof(processStarter));
+        }
+
+        public void Handle(VSTSReleaseTask task)
+        {
+            VSTSRelease_v1 release = (VSTSRelease_v1) task.Parent.Parent;
+            if (release.DefinitionId == null)
+                throw new InvalidOperationException("Cannot open web page. Unknown definition id.");
+
+            var uri = $"{release.Url}/{release.Collection}/{release.Project}/_release?definitionId={release.DefinitionId}&_a=releases";
+
+            _processStarter.Start(uri);
+        }
+    }
+}
